554929fe967d713aaffc1f33c369fc6b9de681d0
OluwaTossin <temitopep450@gmail.com>
2024-06-22 16:26:26 +0100

    Add frontend pages and configuration

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+// frontend/pages/index.js
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My AWS DevOps Project</h1>
+      <p>This is the homepage of the application.</p>
+    </div>
+  );
+};
+
+export default HomePage;
